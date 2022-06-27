effc5b2825db55fe626955ba1b94314714503b3c
Yrian Derreumaux <yderr001@ucr.edu>
2022-06-27 13:28:02 -0700

    added new model with race recoded

--- Scripts/behavioral/Study2.Analyses.R
+++ Scripts/behavioral/Study2.Analyses.R
@@ -2,9 +2,9 @@
 id <- "1hVhWUzRQ--hhPxDhj9rIsvo5-R2wpVHk" # google file ID
 Study2Data <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
 Study2Data$Participant <- as.factor(Study2Data$Participant) #make participant factor
-packrat::on()
-packrat::init()
-packrat::snapshot()
+# packrat::on()
+# packrat::init()
+# packrat::snapshot()
 #create variable for face/cloud and prediction type crime/weather
 Study2Data$stim_type <- NA
 Study2Data$pred_type <- NA
@@ -41,41 +41,62 @@ contrasts(Study2Data$ethnicity_recode) <- contr.treatment(3, base = 1) #make oth
 
 #run mixed models for learning by condition
 ######
-Study2.model<- glmer(acc~scale(Trial)+Condition_dum* (scale(Trial)|Participant)+(1|Face_Shown), data = Study2Data, family = "binomial")
+Study2.model<- glmer(acc~scale(Trial)*Condition_dum+ (scale(Trial)|Participant)+(1|Face_Shown), data = Study2Data, family = "binomial")
 #save(Study2.model, file = "study2model.rda") #function to save model to reload later for simulations
 Study2.coef <- summary(Study2.model) #removing random effect for stimuli removes convergence issue, but does not change estimates and therefore we are keeping it. 
 Study2.effects <- exp(fixef(Study2.model))
 Study2.effects.CI <- log(exp(confint(Study2.model,'Condition_dumsteal', level=0.95)))
-#sensitivity analysis
-model.obser.power <- powerSim(Study2.model, fixed("Condition_dum", "z"), seed = 5, nsim = 800, alpha = .05)
 
+#IMS relate to learning in Crime Face or 
+Study2.model<- glmer(acc~scale(Trial)+Condition_dum*scale(IMS)+ (scale(Trial)|Participant)+(1|Face_Shown), data = Study2Data[which(Study2Data$Condition=="steal" | Study2Data$Condition=="weather_faces"),], family = "binomial")
+summary(Study2.model)
+plot_model(Study2.model, type = "pred", terms = c("IMS", "Condition_dum"))
+ggpredict(Study2.model, c("IMS", "Condition_dum"))%>%plot()+theme_classic()
+
+Study2.model222<- glmer(acc~scale(Trial)+Condition_dum*scale(IMS)+ scale(EMS)+(scale(Trial)|Participant)+(1|Face_Shown), data = Study2Data[which(Study2Data$Condition=="steal" | Study2Data$Condition=="weather_faces"),], family = "binomial")
+summary(Study2.model222)
+ggpredict(Study2.model222, c("Condition_dum", "IMS"))%>%plot()+theme_classic()
+exp(fixef(Study2.model222))
+log(exp(confint(Study2.model222,'Condition_dumweather_faces:scale(IMS)', level=0.95)))
 
 #does race moderate learning?
 Study2.race.model<- glmer(acc~scale(Trial)+Condition_dum* ethnicity_eff+(scale(Trial)|Participant)+(1|Face_Shown), data = Study2Data, family = "binomial")
 Study2.race.coef <- summary(Study2.race.model) #no effect of race
-#with race recoded
+tab_model(Study2.race.model.recoded)
+
+#with race recoded with 3 instead of 5 factors
 Study2.race.model.recoded<- glmer(acc~scale(Trial)+Condition_dum*ethnicity_recode+(scale(Trial)|Participant)+(1|Face_Shown), data = Study2Data, family = "binomial")
 Study2.race.coef <- summary(Study2.race.model.recoded) #still no effect of race
-obser.power <- powerSim(Study2.race.model, compare('~scale(Trial)+Condition_dum* ethnicity_eff+(scale(Trial)|Participant)+(1|Face_Shown)'), alpha = .05)
+tab_model(Study2.race.model.recoded)
 
 #Post-hoc simple contrasts
 study2.contr <- emmeans(Study2.model, "Condition_dum")
 study2.contr.coef <- pairs(study2.contr, adjust = "none")
 study2.contr.eff.size <- pairs(study2.contr, adjust = "none", type = "response")
 
-#compare effect sizes
-#steal compared to steal clouds  0.229% more likely to be incorrect
-#steal compared to weather_faces  0.186% more likely to be incorrect
-#steal compared to weather_faces  0.303% more likely to be incorrect
+#calculate base rate for weather cloud condition
+table(Study2Data$acc[which(Study2Data$Condition=="steal")])
+
+# probability of incorrect in Weather Face = 3970/(15548+3970) = 0.203402
+# probability of incorrect in Crime Face= 4286/(14028+4286) = 0.2340286
+#calculate failure rate (i.e., incorrect prediction) in weather faces relative weather clouds
+
+# (0.814 * 0.2340286) /  1 + (0.814 * 0.2340286) - 0.2340286 = 0.14697 = 14.69 % failure rate in Crime Faces compared to 20.34% in Weather Faces
+
 
 #####
 
-#demonstrating that there are no differences in learning on the first 5 trials
 ######
-first_5_df <- Study2Data[-which(Study2Data$Trial>5),]
-first_5_df.model<- glmer(acc~scale(Trial)+Condition_dum+ (scale(Trial)|Participant)+(1|Face_Shown), data = first_5_df, family = "binomial")
+first_5_df <- Study2Data[which(Study2Data$Trial<125),]
+
+first_5_df.model<- glmer(acc~scale(Trial)*Condition_dum+ (scale(Trial)|Participant)+(1|Face_Shown), data = first_5_df, family = "binomial")
 summary(first_5_df.model)
 plot_model(first_5_df.model, type = "pred", terms = c("Trial", "Condition_dum"))
+
+study2.contr.test <- emmeans(first_5_df.model, "Condition_dum")
+study2.contr.test.coef <- pairs(study2.contr.test, adjust = "none")
+study2.contr.test.size <- pairs(study2.contr.test, adjust = "none", type = "response")
+
 #####
 
 
