e85abf211f8785adb57c44fca67d1a5e98ef3cff
Yrian Derreumaux <50255125+yrianderreumaux@users.noreply.github.com>
2022-07-12 10:41:45 -0700

    remove packrat and use groundhog instead

--- Scripts/behavioral/ParticipantExclusion.R
+++ Scripts/behavioral/ParticipantExclusion.R
@@ -1,7 +1,7 @@
 #Study 1
 #####
 id <- "1pAzA-_qaOdso1NIWYsz89uHXXMuBHHur" # google file ID
-Study1exclus <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+Study1exclus <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id),  header=T, stringsAsFactors = FALSE, na.strings=c("","NA"))
 Study1exclus <- read.csv("data/raw_data/study1Raw_0507.csv", header=T, stringsAsFactors = FALSE, na.strings=c("","NA")) #load data
 Study1exclus$Optimal.Response <- as.integer(as.character(Study1exclus$Optimal.Response))
 averga_acc <- Study1exclus %>%
@@ -36,8 +36,10 @@ table(Study1exclus$STIMULI.1[which(Study1exclus$avg_acc<.52)])
 
 #Study2
 #####
-id <- "1RTDjNCXteFWVrpLVy8pOz5TNSIhHR_KG" # google file ID
-Study2exclus <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+
+id <- "1MK2RM6zavPqwUtrnmCrtRZBpf9oH0zCg" # google file ID
+Study2exclus <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id),  header=T, stringsAsFactors = FALSE, na.strings=c("","NA"))
+
 Study2exclus <- read.csv("data/raw_data/study2Raw.csv", header=T, stringsAsFactors = FALSE, na.strings=c("","NA"))
 #show # of participants under 52% accurate
 study2Outliers <- Study2exclus$Participant[which(Study2exclus$avg_acc<=.52)] #11 participants under 52%
@@ -45,15 +47,7 @@ study2Outliers<- length(unique(study2Outliers))
 #by condition
 Study2exclus_unique <- Study2exclus[!duplicated(Study2exclus$Participant),]
 table(Study2exclus_unique$Condition[which(Study2exclus_unique$avg_acc<.52)])
-#accuracy early on
-Study2_first_10 <- Study2exclus[which(Study2exclus$Trial<40),]
-averga_acc_early <- Study2_first_10 %>%
-  group_by(Condition)%>%
-  dplyr::summarise(early_acc=mean(acc, na.rm = T))
-Study2_first_10 <- merge(Study2_first_10, averga_acc_early, by = c("Participant"), all.x = F)
-describeBy(Study2_first_10$early_acc, Study2_first_10$Condition)
-
-#Total Sample for study 1 after exclusion = 373
+#Total Sample for study 2 after exclusion = 373
 #####
 
 #Study3
@@ -67,7 +61,7 @@ Study3exclusv <- merge(Study3exclusv, averga_acc, by = c("Participant"), all.x =
 #show # of participants under 52% accurate
 study3Outliers <- Study3exclusv$Participant[which(Study3exclusv$avg_acc<=.52)] #15 participants under 52%
 study3Outliers<- length(unique(study3Outliers))
-#Total Sample for study 1 after exclusion = 205
+#Total Sample for study 3 after exclusion = 205
 #####
 
 
